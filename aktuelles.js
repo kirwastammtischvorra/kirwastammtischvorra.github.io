561329b5036c19674d1387d2c5927713050a162c
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2018-03-29 11:59:58 +0200

    Update sidebar

--- aktuelles.js
+++ aktuelles.js
@@ -1,8 +1,9 @@
 document.write(
-    'Fr, 09.02. um 19:00 Uhr:<br/>Hüttenabend. Es wird gegrillt.</dd>' +
-    '<dd>Sa, 04.03. ab 10:00 Uhr:<br/>Gemeindeschie&szlig;en beim Pechwirt.</dd>' +
-    '<dd>Fr, 16.03. um 15:00 Uhr:<br/>Arbeitseinsatz, anschließend Hüttenabend. Es gibt eine Brotzeit.</dd>' +
+    'So, 15.04. um 10:00 Uhr:<br/>Wei&szlig;wurstfr&uuml;hschoppen in der Kirwaboumh&uuml;tte.</dd>' +
+    '<dd>Fr, 27.04. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt Brotzeit.</dd>' +
+    '<dd>Sa, 05.05. um 16:30 Uhr:<br/>Wanderung nach Treuf zu Braun\'s Hofcaf&eacute;. Treffpunkt am Rathaus.</dd>' +
+    '<dd>Fr, 18.05. ab 19:00 Uhr:<br/>Hüttenabend. Es wird gegrillt.</dd>' +
     '<dt>Informationen</dt>' +
     '<dd>Wir sind auf der Suche nach neuen Kirwaboum und Kirwamadler. Wenn ihr Interesse habt bei uns mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a>, <a href="mailto:felix17295@gmx.de">Felix</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>' +
     '<dt></dt>' +
-    '<dd>Zuletzt aktualisiert am 17.03.2018.');
+    '<dd>Zuletzt aktualisiert am 29.03.2018.');
