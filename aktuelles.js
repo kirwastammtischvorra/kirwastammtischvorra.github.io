c7f7b235626cab86204d9c01b66b5b3d7446f6ae
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2018-05-22 11:00:01 +0200

    Update upcoming events

--- aktuelles.js
+++ aktuelles.js
@@ -1,8 +1,7 @@
 document.write(
-    'So, 15.04. um 10:00 Uhr:<br/>Wei&szlig;wurstfr&uuml;hschoppen in der Kirwaboumh&uuml;tte.</dd>' +
-    '<dd>Fr, 27.04. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt Brotzeit.</dd>' +
-    '<dd>Sa, 05.05. um 16:30 Uhr:<br/>Wanderung nach Treuf zu Braun\'s Hofcaf&eacute;. Treffpunkt am Rathaus.</dd>' +
-    '<dd>Fr, 18.05. ab 19:00 Uhr:<br/>Hüttenabend. Es wird gegrillt.</dd>' +
+    'Von 22. bis 24. Juni findet die Kirwa im Schloss statt!' +
+    '<dd>Fr, 06.07. ab 18:00 Uhr:<br/>Helferessen in der Kirwaboumh&uuml;tte.</dd>' +
+    '<dd>Fr, 17.08. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt Schaschliktopf.</dd>' +
     '<dt>Informationen</dt>' +
     '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>' +
     '<dt></dt>' +
