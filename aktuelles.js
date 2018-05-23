5080fee16ab553180619aa95ea63fe6de1f74f5b
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2018-05-23 09:31:55 +0200

    Add Kirwa 2018 flyer and time table

--- aktuelles.js
+++ aktuelles.js
@@ -1,8 +1,8 @@
 document.write(
-    'Von 22. bis 24. Juni findet die Kirwa im Schloss statt!' +
+    'Von 22. bis 24. Juni findet die Kirwa im Schloss statt! Zum <a href="img/flyer_18.png">Programm</a>.' +
     '<dd>Fr, 06.07. ab 18:00 Uhr:<br/>Helferessen in der Kirwaboumh&uuml;tte.</dd>' +
     '<dd>Fr, 17.08. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt Schaschliktopf.</dd>' +
     '<dt>Informationen</dt>' +
     '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>' +
     '<dt></dt>' +
-    '<dd>Zuletzt aktualisiert am 22.05.2018.');
+    '<dd>Zuletzt aktualisiert am 23.05.2018.');
