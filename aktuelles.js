4a2f9f4dcc708eefbcab8432a0ad3ac7030e0be5
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2018-07-11 15:29:33 +0200

    Update upcoming dates

--- aktuelles.js
+++ aktuelles.js
@@ -1,8 +1,8 @@
 document.write(
-    'Von 22. bis 24. Juni findet die Kirwa im Schloss statt!<br/>Zum <a href="img/flyer_18.png" target="_blank">Programm</a>.</dd>' +
-    '<dd>Fr, 06.07. ab 18:00 Uhr:<br/>Helferessen in der Kirwaboumh&uuml;tte.</dd>' +
-    '<dd>Fr, 17.08. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt Schaschliktopf.</dd>' +
-    '<dt>Informationen</dt>' +
-    '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>' +
-    '<dt></dt>' +
-    '<dd>Zuletzt aktualisiert am 23.05.2018.');
+    '<dd>Fr, 17.08. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt Schaschliktopf.</dd>'
+    + '<dd>Fr, 14.09. ab 15:00 Uhr:<br/>Arbeitseinsatz, anschließend Hüttenabend. Es gibt Brotzeit.</dd>'
+    + '<dd>Sa, 22.09. um 12:00 Uhr:<br/>Wanderung in der Fränkischen. Treffpunkt am Rathaus.</dd>'
+    + '<dt>Informationen</dt>'
+    + '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>'
+    + '<dt></dt>'
+    + '<dd>Zuletzt aktualisiert am 11.07.2018.');
