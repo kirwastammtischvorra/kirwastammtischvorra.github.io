a31eaf7a433e578508d3f394784bb5ba1fccc61e
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2018-09-05 18:50:54 +0200

    Postpone date

--- aktuelles.js
+++ aktuelles.js
@@ -1,7 +1,7 @@
 document.write(
     '<dd>Fr, 17.08. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt Schaschliktopf.</dd>'
     + '<dd>Fr, 14.09. ab 15:00 Uhr:<br/>Arbeitseinsatz, anschließend Hüttenabend. Es gibt Brotzeit.</dd>'
-    + '<dd>Sa, 22.09. um 12:00 Uhr:<br/>Wanderung in der Fränkischen. Treffpunkt am Rathaus.</dd>'
+    + '<dd>Sa, 29.09. um 12:00 Uhr:<br/>Wanderung in der Fränkischen. Treffpunkt am Rathaus.</dd>'
     + '<dt>Informationen</dt>'
     + '<dd>In der Ewigen Kirwaliste gibts zwei neue (alte) Gruppenbilder von <a href="//kirwastammtischvorra.github.io/liste/fr/1992.jpg">1992</a> und <a href="//kirwastammtischvorra.github.io/liste/fr/1993.jpg">1993</a> zu sehen.</dd>'
     + '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>'
