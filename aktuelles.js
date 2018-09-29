3e378ceb117f63ac5bc91ed606981e1c35222f28
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2018-09-29 13:53:57 +0200

    Update upcoming dates

--- aktuelles.js
+++ aktuelles.js
@@ -1,6 +1,9 @@
 document.write(
     + 'Sa, 29.09. um 12:00 Uhr:<br/>Wanderung in der Fränkischen. Treffpunkt am Rathaus.</dd>'
     + '<dd>Fr, 12.10. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt ger&auml;ucherte Forellen.</dd>'
+    + '<dd>Sa, 10.11. ab 18:30 Uhr:<br/>Martinifeuer der Feuerwehr am Feuerwehrhaus.</dd>'
+    + '<dd>Fr, 16.11. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt saure Bratwürste.</dd>'
+    + '<dd>Sa, 24.11. ab 18:30 Uhr:<br/>Nachtwanderung zum Schinkenessen beim Pechwirt. Treffpunkt am Rathaus.</dd>'
     + '<dt>Informationen</dt>'
     + '<dd>In der Ewigen Kirwaliste gibts zwei neue (alte) Gruppenbilder von <a href="//kirwastammtischvorra.github.io/liste/fr/1992.jpg">1992</a> und <a href="//kirwastammtischvorra.github.io/liste/fr/1993.jpg">1993</a> zu sehen.</dd>'
     + '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>'
