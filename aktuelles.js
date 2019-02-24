5be7af5949f7f29dff20513a40936ce44694e3c9
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2019-02-24 13:25:51 +0100

    Add dates 2019

--- aktuelles.js
+++ aktuelles.js
@@ -1,8 +1,10 @@
 document.write(
-    'Sa, 22.12. ab 19:00 Uhr:</br>Weihnachtsfeier beim Toni.</dd>'
-    + '<dd>Mi, 02.01.2019 um 19:30 Uhr:</br>Jahreshauptversammlung in der Kirwaboumh&uuml;tte.</dd>'
+    'Fr, 08.03. um 15:00 Uhr:<br/>Arbeitseinsatz, anschließend Hüttenabend. Es gibt eine Brotzeit.</dd>'
+    + '<dd>Sa, 24.03. ab 10:00 Uhr:<br/>Gemeindeschie&szlig;en beim Pechwirt.</dd>'
+    + '<dd>So, 31.03. um 10:00 Uhr:<br/>Wei&szlig;wurstfr&uuml;hschoppen in der Kirwaboumh&uuml;tte.</dd>'
+    + '<dd>Fr, 26.04. ab 19:00 Uhr:<br/>Hüttenabend. Es gibt Brotzeit.</dd>'
     + '<dt>Informationen</dt>'
     + '<dd>In der Ewigen Kirwaliste gibts zwei neue (alte) Gruppenbilder von <a href="//kirwastammtischvorra.github.io/liste/fr/1992.jpg">1992</a> und <a href="//kirwastammtischvorra.github.io/liste/fr/1993.jpg">1993</a> zu sehen.</dd>'
     + '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>'
     + '<dt></dt>'
-    + '<dd>Zuletzt aktualisiert am 27.11.2018.');
+    + '<dd>Zuletzt aktualisiert am 24.02.2019.');
