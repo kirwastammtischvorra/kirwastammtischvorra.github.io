6fbe36b8e7e2924b1b8a3f190a9a5f6feffc9785
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2019-05-30 19:29:30 +0200

    Wording

--- aktuelles.js
+++ aktuelles.js
@@ -1,5 +1,5 @@
 document.write(
-    'Hier gibt es unsere aktuellen <a href="termine.html">Termine</a>.</dd>'
+    'Hier geht es zu unseren aktuellen <a href="termine.html">Terminen</a>.</dd>'
     + '<dt>Informationen</dt>'
     + '<dd>In der Ewigen Kirwaliste gibts zwei neue (alte) Gruppenbilder von <a href="//kirwastammtischvorra.github.io/liste/fr/1992.jpg">1992</a> und <a href="//kirwastammtischvorra.github.io/liste/fr/1993.jpg">1993</a> zu sehen.</dd>'
     + '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>'
