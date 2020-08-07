65006a9566142d4b6b462d938e8faf750ca89e8d
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2020-08-07 16:14:44 +0200

    Add group picture from 1995

--- aktuelles.js
+++ aktuelles.js
@@ -2,7 +2,8 @@ document.write(
     'Hier geht es zu unseren aktuellen <a href="termine.html">Terminen</a>.</dd>'
     + '<dt>Informationen</dt>'
     + '<dd>Neu zum Download und Ausdrucken: <a href="//kirwastammtischvorra.github.io/downloads/Liederbuch Kirwastammtisch.pdf">Liederbuch</a> für unsere Gesangsabende.</dd>'
+    + '<dd>In der Ewigen Kirwaliste gibts ein neues (altes) <a href="//kirwastammtischvorra.github.io/liste/fr/1995.jpg">Gruppenbild von der Kirwa im Jahre 1995</a> zu sehen.</dd>'
     + '<dd>In der Ewigen Kirwaliste gibts ein neues (altes) <a href="//kirwastammtischvorra.github.io/liste/fr/1988.jpg">Gruppenbild von der Kirwa im Jahre 1988</a> zu sehen.</dd>'
     + '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>'
     + '<dt></dt>'
-    + '<dd>Zuletzt aktualisiert am 17.07.2020.');
+    + '<dd>Zuletzt aktualisiert am 07.08.2020.');
