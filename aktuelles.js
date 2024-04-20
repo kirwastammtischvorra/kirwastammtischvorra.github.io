cd2bb5435598fcd746a9a680b8fa47154c2c5c5a
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2024-04-20 11:34:20 +0200

    Add appointments 2024

--- aktuelles.js
+++ aktuelles.js
@@ -6,4 +6,4 @@ document.write(
     + '<dd>In der Ewigen Kirwaliste gibts ein neues (altes) <a href="//kirwastammtischvorra.github.io/liste/fr/1988.jpg">Gruppenbild von der Kirwa im Jahre 1988</a> zu sehen.</dd>'
     + '<dd>Wenn ihr Interesse habt bei den Kirwaboum und -madeln mitzumachen, sei es nur zum Baumaustanzen an der Kirwa oder ihr auch bei den vielen Veranstaltungen im übrigen Jahr dabei sein wollt, dann meldet euch gerne bei <a href="mailto:lara.winter@t-online.de">Lara</a> oder <a href="mailto:patrick.eichenmueller@gmx.de">Patrick</a>. Die können euch sagen, wie es bei uns abl&auml;uft.</dd>'
     + '<dt></dt>'
-    + '<dd>Zuletzt aktualisiert am 08.07.2023.');
+    + '<dd>Zuletzt aktualisiert am 20.04.2024.');
