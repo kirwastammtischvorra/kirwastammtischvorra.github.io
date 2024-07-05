786a1451412666b970f78e30321735cb1f4793d4
Manuel Leimberger <4976409+manu1311421@users.noreply.github.com>
2024-07-05 10:05:55 +0200

    Fix deprecated command convert->magick

--- liste/resize_image_liste.sh
+++ liste/resize_image_liste.sh
@@ -3,8 +3,8 @@
 for i in "$@"; do 
     cp ./fr/$i ./tn/$i
     cp ./fr/$i ./gruppenf.jpg
-    convert ./tn/$i -resize 220x220 -quality 75 ./tn/$i
-    convert ./gruppenf.jpg -resize 1200x1200 -quality 65 ./gruppenf.jpg
+    magick ./tn/$i -resize 220x220 -quality 75 ./tn/$i
+    magick ./gruppenf.jpg -resize 1200x1200 -quality 65 ./gruppenf.jpg
     exiftool -overwrite_original -all:all= -tagsFromFile @ -exif:Orientation ./fr/$i &>/dev/null
     exiftool -overwrite_original -all:all= -tagsFromFile @ -exif:Orientation ./tn/$i &>/dev/null
     exiftool -overwrite_original -all:all= -tagsFromFile @ -exif:Orientation ./gruppenf.jpg &>/dev/null
